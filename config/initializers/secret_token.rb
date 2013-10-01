ebc5202cc3e390b37c6db9bf42adf6fed8df55bf
Thomas Scherz <thomas.scherz@uc.edu>
2013-10-01 15:22:09 -0400

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CwruCamp::Application.config.secret_key_base = 'd4ae74526fee73d999130bf97a16e5fce4609e742b3fbfabd7e0457a8f8ed0aef2b227e538333650e97daa08e4460e2e413858ecbb567c5601a3b894f29917da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CwruCamp::Application.config.session_store :cookie_store, key: '_cwru_camp_session'
